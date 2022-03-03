08c6abb0c9da574699e25a19315a239bd0f51bb7
Emre Ünsal <unsalemre111@gmail.com>
2022-03-04 00:57:44 +0300

    web socket attempts were made

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
 const { indexRouter } = require('./router/indexRouter');
 const { connectDB } = require('./database/connectToDatabase');
 
 const app = express();
 
-app.listen(3000);
+const server = app.listen(3000);
+const io = socket(server);
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
+
 app.use(express.json());
 connectDB();
 app.use('/', indexRouter);
+app.get('/', (req, res) => {
+  res.sendFile('./web/index.html', { root: __dirname });
+});

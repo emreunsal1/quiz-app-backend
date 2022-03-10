21b051977fe37ff7cdfafe1a3e8f643bd766e1be
Emre Ünsal <unsalemre111@gmail.com>
2022-03-10 14:15:55 +0300

    caught webSocket

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const socket = require('socket.io');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const { indexRouter } = require('./router/indexRouter');
 const { connectDB } = require('./database/connectToDatabase');
@@ -8,22 +9,26 @@ const app = express();
 const http = require('http').Server(app);
 
 app.use(express.json());
-const users = [];
-const io = socket(http);
-io.on('connection', (socket) => {
-  socket.on('joinRoom', (data) => {
-    socket.join(data.roomKey);
-    users.push({ name: data.name, id: socket.id, roomKey: data.roomKey });
-    const filtersUsers = users.filter((user) => user.roomKey === data.roomKey);
-    io.to(data.roomKey).emit('studentJoined', filtersUsers);
-  });
+app.use(cors());
 
-  socket.on('createRoom', (data) => {
-    const roomKey = data.roomKey;
-    socket.join(roomKey);
-    io.emit('roomCreated', { roomKey });
-  });
-});
+// const users = [];
+// const io = socket(http);
+// io.on('connection', (socket) => {
+//   socket.on('joinRoom', (data) => {
+//     socket.join(data.roomKey);
+//     users.push({ name: data.name, id: socket.id, roomKey: data.roomKey });
+//     const filtersUsers = users.filter((user) => user.roomKey === data.roomKey);
+//     io.to(data.roomKey).emit('studentJoined', filtersUsers);
+//     const userId = io.sockets.adapter.rooms.get(data.roomKey);
+//     console.log(userId);
+//   });
+
+//   socket.on('createRoom', (data) => {
+//     const roomKey = data.roomKey;
+//     socket.join(roomKey);
+//     io.emit('roomCreated', { roomKey });
+//   });
+// });
 
 connectDB();
 app.use('/', indexRouter);
